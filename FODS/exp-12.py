8230264209aa6bf6bac6ab5981aaa1940cc5852f
192472144- A. SAI ROHIT <sairohit399@gmail.com>
2026-08-19 11:31:47 +0530

    exp-12.py
    Added comments to clarify the purpose of the temperature and rainfall plots.

--- FODS/exp-12.py
+++ FODS/exp-12.py
@@ -1,3 +1,5 @@
+# 1. Develop a Python program to create a line plot of the monthly temperature data.
+# 2: Develop a Python program to create a scatter plot of the monthly rainfall data.
 import pandas as pd
 import matplotlib.pyplot as plt
 from tkinter import Tk
