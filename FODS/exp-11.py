1402d4a4abdba341fc461acf3357c68047d9650b
192472144- A. SAI ROHIT <sairohit399@gmail.com>
2026-08-19 11:30:50 +0530

    exp-11.py
    Updated the script to include comments outlining the tasks for creating line, scatter, and bar plots using Matplotlib.

--- FODS/exp-11.py
+++ FODS/exp-11.py
@@ -1,3 +1,7 @@
+# 1. Write code to create a simple line plot in Python using Matplotlib to predict sales happened in a
+# month?
+# 2. Write code to create a scatter plot in Python using Matplotlib to predict sales happened in a month?
+# 3. Develop a Python program to create a bar plot of the monthly sales data.
 import pandas as pd
 import matplotlib.pyplot as plt
 from tkinter import Tk
